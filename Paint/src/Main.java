10e54cf527a5c49382afb4437e2263ebc3daffd8
PinchukAA <juggler7@mail.ru>
2016-02-22 00:11:00 +0300

    hint size

--- Paint/src/Main.java
+++ Paint/src/Main.java
@@ -2,9 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class Main {
     JButton  btCLear, btRed, btOrange, btYellow, btGreen, btBlue,btMagenta, btGray, btBlack, btChColor ,btErase;
+    JMenuItem x1Item, x2Item, x4Item, x8Item, x1ErItem, x2ErItem, x4ErItem, x8ErItem;
     Paper paper;
     Color c;
     ActionListener actionListener = new ActionListener() {
@@ -40,6 +43,29 @@ public class Main {
             paper.chooseColor(c);
         }
     }
+
+    ActionListener sizeHintActionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == x1Item){
+                paper.x1();
+            } else if (e.getSource() == x2Item) {
+                paper.x2();
+            } else if (e.getSource() == x4Item) {
+                paper.x4();
+            } else if (e.getSource() == x8Item) {
+                paper.x8();
+            } else if (e.getSource() == x1ErItem){
+                paper.x1();
+            } else if (e.getSource() == x2ErItem) {
+                paper.x2();
+            } else if (e.getSource() == x4ErItem) {
+                paper.x4();
+            } else if (e.getSource() == x8ErItem) {
+                paper.x8();
+            }
+        }
+    };
 
     public static void main(String[] args){
         new Main().show();
@@ -84,13 +110,39 @@ public class Main {
         toolsMenu.add(hintMenu);
         JMenuItem colorItem = new JMenuItem("Цвет кисти");
         hintMenu.add(colorItem);
+        colorItem.addActionListener(new colorButtonActionListener());
         JMenu hintSizeMenu = new JMenu("Размер кисти");
         hintMenu.add(hintSizeMenu);
+        x1Item = new JMenuItem("x1");
+        hintSizeMenu.add(x1Item);
+        x1Item.addActionListener(sizeHintActionListener);
+        x2Item = new JMenuItem("x2");
+        hintSizeMenu.add(x2Item);
+        x2Item.addActionListener(sizeHintActionListener);
+        x4Item = new JMenuItem("x4");
+        hintSizeMenu.add(x4Item);
+        x4Item.addActionListener(sizeHintActionListener);
+        x8Item = new JMenuItem("x8");
+        hintSizeMenu.add(x8Item);
+        x8Item.addActionListener(sizeHintActionListener);
 
         JMenu eraserMenu = new JMenu("Ластик");
         toolsMenu.add(eraserMenu);
         JMenu eraserSizeMenu= new JMenu("Размер ластика");
         eraserMenu.add(eraserSizeMenu);
+        x1ErItem = new JMenuItem("x1");
+        eraserSizeMenu.add(x1ErItem);
+        x1ErItem.addActionListener(sizeHintActionListener);
+        x2ErItem = new JMenuItem("x2");
+        eraserSizeMenu.add(x2ErItem);
+        x2ErItem.addActionListener(sizeHintActionListener);
+        x4ErItem = new JMenuItem("x4");
+        eraserSizeMenu.add(x4ErItem);
+        x4ErItem.addActionListener(sizeHintActionListener);
+        x8ErItem = new JMenuItem("x8");
+        eraserSizeMenu.add(x8ErItem);
+        x8Item.addActionListener(sizeHintActionListener);
+
 
         JMenu textMenu = new JMenu("Текст");
         toolsMenu.add(textMenu);
@@ -178,7 +230,7 @@ public class Main {
         tools.add(btErase);
 
         content.add(tools, BorderLayout.NORTH);
-        frame.setSize(1920, 1080);
+        frame.setSize(800, 600);
       //  frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
